41bd8d3a94b5597a26a1b7f75e374f172e4275e8
RebecaRakotomalala <ranjarakoto0@gmail.com>
2025-07-16 11:05:41 +0300

    data

---
+++ appreact/src/ThemeContext.js
@@ -0,0 +1,25 @@
+// src/ThemeContext.js
+import { createContext, useContext, useState, useEffect } from 'react';
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+  };
+
+  // Appliquer une classe CSS au body
+  useEffect(() => {
+    document.body.className = theme === 'light' ? 'light-theme' : 'dark-theme';
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
