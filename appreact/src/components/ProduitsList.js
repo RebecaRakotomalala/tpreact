2c34d702d031e39182abbc60bebdabdcd1463e21
RotsyMandrindra <rotsy.rasoanaivo@gmail.com>
2025-07-16 10:31:40 +0300

    fix: import useNavigate

--- appreact/src/components/ProduitsList.js
+++ appreact/src/components/ProduitsList.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './VendeursList.css'; 
+import { useNavigate } from 'react-router-dom';
 
 function ProduitsList() {
   const [produits, setProduits] = useState([]);
